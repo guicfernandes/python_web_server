678eb0ac3c9540190cf3c380fb93bcd32b835313
Guilherme <guilhermecfernandes@hotmail.com>
2022-06-06 18:14:23 -0300

    adding styling, scripting and url parameters

--- app/server.py
+++ app/server.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    return render_template('./index.html', name=username, id=post_id)
+    # return "<p>Hello, World!</p>"
+
+
+@app.route("/about")
+def about():
+    return render_template('./about.html')
+
+
+@app.route("/blog")
+def blog():
+    return "<p>Some of people's thoughts!</p>"
