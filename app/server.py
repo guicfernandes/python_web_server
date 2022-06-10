1d6bd370ecd1de9910e136f6edeaaab007ad698d
Guilherme <guilhermecfernandes@hotmail.com>
2022-06-10 08:20:21 -0300

    using free templates do the website

--- app/server.py
+++ app/server.py
@@ -3,17 +3,27 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template('./index.html', name=username, id=post_id)
+@app.route("/")
+@app.route("/index.html")
+def my_home():
+    return render_template('./index.html')
     # return "<p>Hello, World!</p>"
 
 
-@app.route("/about")
-def about():
-    return render_template('./about.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route("/blog")
-def blog():
-    return "<p>Some of people's thoughts!</p>"
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
