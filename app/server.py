40d515b8c017ca9edfda91a6bdc370c1aca0ce4f
Guilherme <guilhermecfernandes@hotmail.com>
2022-06-10 18:46:59 -0300

    working on submit forms using flask

--- app/server.py
+++ app/server.py
@@ -17,13 +17,4 @@ def html_page(page_name):
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+    return 'form submitted!'
